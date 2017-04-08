5a330d6686ece79c16a942adb65a1ffe025c55bf
Jennifer Georgevich <jenngeorgevich@gmail.com>
2017-04-08 14:06:21 -0700

    Fix protect_from_forgery error

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
+protect_from_forgery prepend: true
 respond_to :json
 
   # GET /resource/sign_in
