46fe91b41ed5dfbaa2a6d52112b2fae091c13c19
Jennifer Georgevich <jenngeorge@berkeley.edu>
2017-08-09 17:49:35 -0400

    Add neuro cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,11 +46,6 @@ javascript = Subject.create!(
   title: "Javascript"
 )
 
-cognitive_biases = Subject.create!(
-  user_id: user1.id,
-  title: "Cognitive Biases"
-)
-
 cognitive_science = Subject.create!(
   user_id: user1.id,
   title: "Cognitive Science"
@@ -61,12 +56,18 @@ spanish = Subject.create!(
   title: "Spanish"
 )
 
+cantonese = Subject.create!(
+  user_id: user1.id,
+  title: "Cantonese"
+)
+
 # users following subjects
 SubjectFollow.create!(user_id: guest.id, subject_id: javascript.id)
 SubjectFollow.create!(user_id: guest.id, subject_id: perception_science.id)
 SubjectFollow.create!(user_id: guest.id, subject_id: neuroscience.id)
 SubjectFollow.create!(user_id: guest.id, subject_id: cognitive_science.id)
 SubjectFollow.create!(user_id: guest.id, subject_id: spanish.id)
+SubjectFollow.create!(user_id: guest.id, subject_id: cantonese.id)
 
 # decks
 
@@ -76,9 +77,21 @@ functional_modules = Deck.create!(
   subject_id: neuroscience.id
 )
 
-neurons = Deck.create!(
+muscles = Deck.create!(
   user_id: user1.id,
-  title: "Neurons",
+  title: "Muscles",
+  subject_id: neuroscience.id
+)
+
+sleep = Deck.create!(
+  user_id: user1.id,
+  title: "Sleep",
+  subject_id: neuroscience.id
+)
+
+memory = Deck.create!(
+  user_id: user1.id,
+  title: "Memory",
   subject_id: neuroscience.id
 )
 
@@ -104,24 +117,6 @@ touch = Deck.create!(
   user_id: user1.id,
   title: "Touch",
   subject_id: perception_science.id
-)
-
-common_fallacies = Deck.create!(
-  user_id: user1.id,
-  title: "Common Fallacies",
-  subject_id: cognitive_biases.id
-)
-
-common_statistical_errors = Deck.create!(
-  user_id: user1.id,
-  title: "Common Statistical Errors",
-  subject_id: cognitive_biases.id
-)
-
-landmark_studies = Deck.create!(
-  user_id: user1.id,
-  title: "Landmark Studies",
-  subject_id: cognitive_biases.id
 )
 
 linguistics = Deck.create!(
@@ -177,6 +172,8 @@ body_and_clothing = Deck.create!(
   title: "Body and Clothing",
   subject_id: spanish.id
 )
+
+
 # create cards for each deck from card txt files
 decks = Deck.all
 decks.each do |deck|
